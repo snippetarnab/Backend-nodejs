3820cc3527ecbbd6e1b2127e8f52630bb12baf69
snippetarnab <arnabvivo2020@gmail.com>
2024-11-22 23:29:58 +0530

    Connecting to Mongoose

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.db.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+//Another approch of connecting the database
+/*
+import { DB_NAME } from "./constants.js";
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("Error", (error) => {
+      console.log("error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+})();
+*/
